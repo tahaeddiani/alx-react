67580fabbac385ab7f6f1fc60cad455c3ee69adc
Taha ED <73260002+tahaeddiani@users.noreply.github.com>
2024-02-01 15:09:39 +0100

    t

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,14 +1,38 @@
-// webpack.config.js
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
   },
-  resolve: {
-    modules: [path.resolve(__dirname, 'node_modules')],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
   },
 };
